5626c385911eb1263f949e4115aa6014d0ecfb9a
Velasqj8 <jv_619@yahoo.com>
2022-05-14 18:22:13 -0700

    backend structure complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+cats = [
+    {   
+        name: 'Lola',
+        age: 2,
+        enjoys: 'Drinking milk.',
+        image: 'https://images.unsplash.com/photo-1571566882372-1598d88abd90?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+    },
+    {   
+        name: 'Nick',
+        age: 4,
+        enjoys: 'Running around outside.',
+        image: 'https://images.unsplash.com/photo-1503777119540-ce54b422baff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80'
+    },
+    {   
+        name: 'Missy',
+        age: 3,
+        enjoys:' Playing with her toy worm.',
+        image: 'https://images.unsplash.com/photo-1548247416-ec66f4900b2e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80'
+    },
+]
+
+cats.each do |each_cat|
+    Cat.create each_cat
+    p "creating cats #{each_cat}" 
+end
